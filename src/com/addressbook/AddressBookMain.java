eff8ea0f376a6fc283696050daa102d5ac830a39
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-06 18:29:16 +0530

    method to Edit the details

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookMain {
+        ArrayList<Contacts> book = new ArrayList<>();
+        static Scanner sc = new Scanner(System.in);
         public void addDetails()
         {
-                ArrayList<Contacts> book = new ArrayList<>();
+
                 Scanner sn = new Scanner(System.in);
                 Contacts details = new Contacts();
                 System.out.println("Enter The FirstName Name Of Person");
@@ -30,6 +32,61 @@ public class AddressBookMain {
 
                 book.add(details);
                 System.out.println(book);
+        }
+
+        public void editDetails() {
+
+                System.out.println("Confirm your first name to edit details: ");
+                String name = sc.next();
+
+                for (int i = 0; i < book.size(); i++) {
+                        if (book.get(i).getFirstname().equals(name)) {
+                                System.out.println("Select form below to change: ");
+                                System.out.println(
+                                        "\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Email\n7.Zip\n8.Phone number");
+                                int e = sc.nextInt();
+
+                                switch (e) {
+                                        case 1:
+                                                System.out.println("Enter first name");
+                                                book.get(i).setFirstname(sc.next());
+                                                break;
+                                        case 2:
+                                                System.out.println("Enter Last name");
+                                                book.get(i).setLastname(sc.next());
+                                                break;
+                                        case 3:
+                                                System.out.println("Enter address");
+                                                book.get(i).setAddress(sc.next());
+                                                break;
+                                        case 4:
+                                                System.out.println("Enter city");
+                                                book.get(i).setCity(sc.next());
+                                                break;
+                                        case 5:
+                                                System.out.println("Enter state");
+                                                book.get(i).setState(sc.next());
+                                                break;
+                                        case 6:
+                                                System.out.println("Enter email");
+                                                book.get(i).setEmail(sc.next());
+                                                break;
+                                        case 7:
+                                                System.out.println("Enter Zip");
+                                                book.get(i).setZip(sc.nextInt());
+                                                break;
+                                        case 8:
+                                                System.out.println("Enter phone number");
+                                                book.get(i).setPhonenumber(sc.nextInt());
+                                                break;
+                                }
+
+                                System.out.println(book);
+
+                        } else
+                                System.out.println("Enter valid First name");
+                }
+
         }
         public static void main(String[] args) {
                 System.out.println("Welcome to Address Book Program in AddressBookMain");
